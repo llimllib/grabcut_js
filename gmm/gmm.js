7100ea00145a47f71e771afb6294f260972a9a15
Bill Mill <bill.mill@gmail.com>
2009-10-12 23:03:14 -0400

    we now have normally distributed numbers and a chart of them

---
+++ gmm/gmm.js
@@ -0,0 +1,23 @@
+function NormalDistribution(sigma, mu) {
+    return new Object({
+        sigma: sigma,
+        mu: mu,
+        sample: function() {
+            var res;
+            if (this.storedDeviate) {
+                res = this.storedDeviate * this.sigma + this.mu;
+                this.storedDeviate = null;
+            } else {
+                var dist = Math.sqrt(-1 * Math.log(Math.random()));
+                var angle = 2 * Math.PI * Math.random();
+                this.storedDeviate = dist*Math.cos(angle);
+                res = dist*Math.sin(angle) * this.sigma + this.mu;
+            }
+            return res;
+        },
+        sampleInt : function() {
+            return Math.round(this.sample());
+        }
+    });
+}
+
